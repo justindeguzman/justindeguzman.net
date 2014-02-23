8ea1ed1d80a7da9f7071b76c878cc0cccdeb2746
Justin de Guzman <justin@justindeguzman.net>
2014-02-23 16:26:30 -0500

    changed vendor path

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,8 +38,10 @@ module.exports = function(grunt) {
           },
           {
             expand: true,
+            flatten: true,
             src: 'bower_components/**',
-            dest: 'public_html'
+            dest: 'public_html/vendor',
+            filter: 'isFile'
           }
         ]
       }
